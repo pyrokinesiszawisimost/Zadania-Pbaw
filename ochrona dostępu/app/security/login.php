7e8bcabe92b786d20f27672e298ae31c49e670eb
•wine• <147995554+pyrokinesiszawisimost@users.noreply.github.com>
2024-03-19 00:03:41 +0100

    Create login.php

---
+++ ochrona dostępu/app/security/login.php
@@ -0,0 +1,61 @@
+<?php
+require_once dirname(__FILE__).'/../../config.php';
+
+//pobranie parametrów
+function getParamsLogin(&$form){
+	$form['login'] = isset ($_REQUEST ['login']) ? $_REQUEST ['login'] : null;
+	$form['pass'] = isset ($_REQUEST ['pass']) ? $_REQUEST ['pass'] : null;
+}
+
+//walidacja parametrów z przygotowaniem zmiennych dla widoku
+function validateLogin(&$form,&$messages){
+	
+	if ( ! (isset($form['login']) && isset($form['pass']))) {
+		
+		return false;
+	}
+
+	// sprawdzenie, czy potrzebne wartości zostały przekazane
+	if ( $form['login'] == "") {
+		$messages [] = 'Nie podano loginu';
+	}
+	if ( $form['pass'] == "") {
+		$messages [] = 'Nie podano hasła';
+	}
+
+	//nie ma sensu walidować dalej, gdy brak parametrów
+	if (count ( $messages ) > 0) return false;
+
+	// sprawdzenie, czy dane logowania są poprawne
+	// - takie informacje najczęściej przechowuje się w bazie danych
+	//   jednak na potrzeby przykładu sprawdzamy bezpośrednio
+	if ($form['login'] == "admin" && $form['pass'] == "admin") {
+		session_start();
+		$_SESSION['role'] = 'admin';
+		return true;
+	}
+	if ($form['login'] == "user" && $form['pass'] == "user") {
+		session_start();
+		$_SESSION['role'] = 'user';
+		return true;
+	}
+	
+	$messages [] = 'Niepoprawny login lub hasło';
+	return false; 
+}
+
+//inicjacja potrzebnych zmiennych
+$form = array();
+$messages = array();
+
+// pobierz parametry i podejmij akcję
+getParamsLogin($form);
+
+if (!validateLogin($form,$messages)) {
+	
+	include _ROOT_PATH.'/app/security/login_view.php';
+} else { 
+	/
+	header("Location: "._APP_URL);
+	
+}
