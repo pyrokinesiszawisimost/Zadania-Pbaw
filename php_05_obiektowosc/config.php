ebf5dc04d323975cc7b6af802c55c6dd7e3b46e6
•wine• <147995554+pyrokinesiszawisimost@users.noreply.github.com>
2025-03-31 22:39:15 +0200

    Update config.php

--- php_05_obiektowosc/config.php
+++ php_05_obiektowosc/config.php
@@ -6,6 +6,6 @@ $conf = new Config();
 $conf->root_path = dirname(__FILE__);
 $conf->server_name = 'localhost:80';
 $conf->server_url = 'http://'.$conf->server_name;
-$conf->app_root = 'pbaw/lab4_obiektowosc';
+$conf->app_root = '/php_05_uproszczony';
 $conf->app_url = $conf->server_url.$conf->app_root;
 ?>
