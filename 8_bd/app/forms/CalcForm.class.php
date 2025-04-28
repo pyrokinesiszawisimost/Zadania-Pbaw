b060842df8a3d783a072ab583c95da5084bd1f53
•wine• <147995554+pyrokinesiszawisimost@users.noreply.github.com>
2025-04-28 14:05:16 +0200

    Add files via upload

---
+++ 8_bd/app/forms/CalcForm.class.php
@@ -0,0 +1,9 @@
+<?php
+namespace app\forms;
+
+class CalcForm {
+    public ?string $loanAmount = null;
+    public ?string $loanPeriod = null;
+    public ?string $interestRate = null;
+}
+?>
---
+++ 8_bd/app/forms/PersonEditForm.class.php
@@ -0,0 +1,9 @@
+<?php
+namespace app\forms;
+
+class PersonEditForm {
+    public $id;
+    public $name;
+    public $surname;
+    public $birthdate;
+}
